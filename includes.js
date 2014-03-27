9af177fa796e96bfc3b47c8fe06c7de26797d8ef
jt-zoovy <jt@zoovy.com>
2014-03-27 12:50:14 -0700

    polyfill for map

--- includes.js
+++ includes.js
@@ -1,6 +1,40 @@
 //this file merges three formerly separate files: variations.js, json2.js and wiki.js
 
 
+// POLYFILL FOR MAP.  required for peg.
+if (!Array.prototype.map)
+{
+  Array.prototype.map = function(fun /*, thisArg */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var res = new Array(len);
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++)
+    {
+      // NOTE: Absolute correctness would demand Object.defineProperty
+      //       be used.  But this method is fairly new, and failure is
+      //       possible only if Object.prototype or Array.prototype
+      //       has a property |i| (very unlikely), so use a less-correct
+      //       but more portable alternative.
+      if (i in t)
+        res[i] = fun.call(thisArg, t[i], i, t);
+    }
+
+    return res;
+  };
+}
+
+
+
 
 /*
 ############################################################
@@ -1599,6 +1633,7 @@ if (!this.JSON) {
             return str('', {'': value});
         };
     }
+
 
 // remove the JSON parse function.
 
